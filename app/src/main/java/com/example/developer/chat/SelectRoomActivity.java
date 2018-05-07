629d69744cd9a91a32f9e0dacd916b77e0d039bc
e.konyshev@worksolutions.ru <e.konyshev@worksolutions.ru>
2018-05-07 18:34:32 +0300

    show AddRoomDialog

--- app/src/main/java/com/example/developer/chat/SelectRoomActivity.java
+++ app/src/main/java/com/example/developer/chat/SelectRoomActivity.java
@@ -1,8 +1,10 @@
 package com.example.developer.chat;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -15,6 +17,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SelectRoomActivity extends AppCompatActivity implements AddRoomDialogFragment.NoticeDialogListener {
+
+    private static final String DIALOG_ADD_ROOM = "dialog_check_phrase";
 
     Button button;
     RecyclerView recyclerView;
@@ -37,7 +41,9 @@ public class SelectRoomActivity extends AppCompatActivity implements AddRoomDial
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SelectRoomActivity.this, MainActivity.class);
+                DialogFragment dialog = AddRoomDialogFragment.newInstance();
+                dialog.show(getSupportFragmentManager(), DIALOG_ADD_ROOM);
+//                Intent intent = new Intent(SelectRoomActivity.this, MainActivity.class);
                 rooms.add(roomName);
             }
         });
